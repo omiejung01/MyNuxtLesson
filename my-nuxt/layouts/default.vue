88537699d0013fc68bdfb8ef42f3f697f3aee05f
Dhirachat Chayaporn <omiejung@gmail.com>
2023-06-29 12:16:39 +0700

    Node JS Class #2

---
+++ my-nuxt/layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <header>
+            <nav>
+                <NuxtLink to="/">Online Catalog</NuxtLink>
+                <ul>
+                    <li><NuxtLink to="/">Home</NuxtLink></li>
+                    <li><NuxtLink to="/about">About</NuxtLink></li>
+                    <li><NuxtLink to="/products">Products</NuxtLink></li>
+                </ul>
+            </nav>
+        </header>
+
+        <!-- page content go here -->
+        <div>
+            <slot />
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .router-link-exact-active {
+        color: #12b488;
+    }
+</style>
---
+++ my-nuxt/layouts/products.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <header>
+            <nav>
+                <NuxtLink to="/products">Products</NuxtLink>
+            </nav>
+        </header>
+
+        <!-- page content go here -->
+        <div>
+            <slot />
+        </div>
+
+        <footer>
+            <ul>
+                <li><NuxtLink to="/">Home</NuxtLink></li>
+                <li><NuxtLink to="/about">About</NuxtLink></li>
+                <li><NuxtLink to="/products">Products</NuxtLink></li>
+            </ul>
+        </footer>
+    </div>
+</template>
+
+<style scoped>
+    .router-link-exact-active {
+        color: #12b488;
+    }
+</style>
