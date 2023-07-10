13fe849bc1aac2e5f4c328968bb614a76bfc0e21
Dhirachat Chayaporn <omiejung@gmail.com>
2023-07-11 00:12:13 +0700

    Fetch data

--- my-nuxt/pages/products/[id].vue
+++ my-nuxt/pages/products/[id].vue
@@ -1,11 +1,15 @@
 <template>
     <div>
-        <p>Product details for {{ id }}</p>
+        <ProductDetails :product="product" />
     </div>
 </template>
 
 <script setup>
     const { id } = useRoute().params
+    const uri = 'https://fakestoreapi.com/products/' + id
+
+    // fetch singlle product
+    const { data: product } = await useFetch(uri, { key: id})
 
     definePageMeta({
         layout:'products'
