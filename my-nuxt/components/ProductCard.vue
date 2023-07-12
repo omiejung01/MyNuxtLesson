1e0e417e40059abada7e812b03cb0d3efbb17403
Dhirachat Chayaporn <omiejung@gmail.com>
2023-07-13 00:50:02 +0700

    Error Page

--- my-nuxt/components/ProductCard.vue
+++ my-nuxt/components/ProductCard.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="card text-center">
         <img :src="product.image" alt="product thumb" class="thumb">
-        <p class="font-bolld text-gray-500 m-4 truncate">{{ product.title }}</p>
+        <p class="font-bold text-gray-500 m-4 truncate">{{ product.title }}</p>
         <NuxtLink :to="`/products/${product.id}`">
             <p class="btn my-4">View Details</p>
         </NuxtLink>
